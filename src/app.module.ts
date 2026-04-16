39595c7b85900652c80473b28d0fe0145e32fb6f
gabri <gabrielrjed8@gmail.com>
2026-04-15 22:51:16 -0300

    preparando arquivos para o deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,17 @@ import {TemaModule} from './tema/tema.modulo'
 import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	  useClass: ProdService,
+    imports: [ConfigModule],
+}),
     PostagemModule,
     TemaModule,
     AuthModule,
