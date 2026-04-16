113da7859302f53334c3993a227d069c9c8f71b5
gabri <gabrielrjed8@gmail.com>
2026-04-15 23:44:51 -0300

    Atualizacao do Deploy - Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 
 @Module({
